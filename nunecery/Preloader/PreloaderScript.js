8e00313818599390b58c66f9f7496067995d0c63
RIFAT AL-ASHWAD <60539161+rifat32@users.noreply.github.com>
2021-01-21 17:38:29 +0600

    Delete nunecery directory

--- nunecery/Preloader/PreloaderScript.js
+++
@@ -1,6 +0,0 @@
-export default function script() {
-    window.$(window).on('load', function () {
-        window.$(".loader").fadeOut();
-        window.$("#preloder").delay(200).fadeOut("slow");
-    });
-}
